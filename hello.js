9c053793c0ca104e55e9b15fd5e1fa14dd3c142d
David Greenfield <dgreenfield@constantcontact.com>
2014-08-10 02:10:08 -0700

    Finished node tutorial, intersting

---
+++ hello.js
@@ -0,0 +1,13 @@
+exports.world = function() {
+  console.log('Hello World');
+
+
+var http = require('http');
+var fs = require('fs');
+
+var file = fs.createWriteStream("file.jpg");
+var request = http.get("http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg", function(response) {
+  response.pipe(file);
+});
+
+}
